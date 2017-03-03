e6a63d81f87206a66416b8a1d68bf3b97b550c19
Jamie Estill <jaestill@med.umich.edu>
2017-03-03 06:13:01 -0500

    added shell script to get subsets of individual patients

---
+++ scripts/subset_patients.sh
@@ -0,0 +1,79 @@
+cat *sentiment*.csv | grep ^50088 > user_50088_sentimentFeedback.csv
+cat *foodDiary*.csv | grep ^50088 > user_50088_foodDiary.csv
+cat *allergyConsumption*.csv | grep ^50088 > user_50088_allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^5983, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^5983, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^5983, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^5980, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^5980, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^5980, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^5981, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^5981, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^5981, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^5986, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^5986, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^5986, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^5987, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^5987, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^5987, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^5984, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^5984, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^5984, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^5985, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^5985, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^5985, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^79236, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^79236, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^79236, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^194080, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^194080, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^194080, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^79237, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^79237, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^79237, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^194081, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^194081, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^194081, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^97159, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^97159, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^97159, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^97158, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^97158, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^97158, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^97153, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^97153, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^97153, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^97152, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^97152, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^97152, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^97151, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^97151, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^97151, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^97150, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^97150, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^97150, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^97157, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^97157, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^97157, >> user__allergyConsumption.csv
+
+#cat *sentiment*.csv | grep ^97156, >> users__sentimentFeedback.csv
+#cat *foodDiary*.csv | grep ^97156, >> users__foodDiary.csv
+#cat *allergyConsumption*.csv | grep ^97156, >> user__allergyConsumption.csv
