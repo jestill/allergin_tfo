340f47b45eed6f136719358f1bad8de3c85ad203
Jamie Estill <jaestill@med.umich.edu>
2017-03-03 07:18:53 -0500

    Updated food 2 ingredient converter.

--- scripts/cnv_food2ingredients.pl
+++ scripts/cnv_food2ingredients.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl -w
 # Convert the food data to list format
 
-#use Getopt::Std;               # Allows options flags at command line
+# usage as:
+# cnv_food2ingredients.pl - infile.csv -outfile.csv
+
 use Getopt::Long;
 
 my $infile;
@@ -33,9 +35,40 @@ else {
 while ( <TXTIN> ) {
     
     chomp;
-    my @in_cols = split (/\,/, $_);
+    my @in_cols = split (/\t/, $_);
+    my $num_cols = @in_cols;
+
+    # Get ingredients data from sixth column
+    my $ingredient_col = $in_cols[5];
+    my $len_ingredients = length($ingredient_col);
+    my $food_name = $in_cols[1];
     
-    print STDERR $_;
-    print STDERR "\n";
+    # Only print output if we have ingredients listed for the food
+    if ($len_ingredients > 0) {
+	#print STDERR "\t".$ingredient_col."\n";
+
+	# split ingredients list by comma
+	my @ingredients = split (/,/, $ingredient_col);
+	my $num_ingredients = @ingredients;
+	print STDERR $food_name."\n";
+	print STDERR "\tNum ingredients".$num_ingredients."\n";
+	
+	foreach my $ingredient (@ingredients) {
+
+	    # remove any white spaces at beginning
+	    $ingredient =~ s/^\s+//;
+	    # remove any white spaces at the end
+	    $ingredient =~ s/\s+$//;
+	    # put name in lowercase
+	    $ingredient = lc($ingredient);
+	    
+	    print STDERR "\t\t".$ingredient."\n";
+
+	    # print result to output file
+	    print TXTOUT $food_name."\t".$ingredient."\n";
+	}
+
+    }
     
 }
+
